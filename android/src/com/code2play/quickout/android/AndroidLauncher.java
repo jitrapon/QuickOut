f9c2e510cf82216f6383b38b73f62beb27d2cb2b
Jitrapon Tiachunpun <Yoshi3003@gmail.com>
2014-04-14 13:26:40 +0700

    project init files

---
+++ android/src/com/code2play/quickout/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.code2play.quickout.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.code2play.quickout.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
---
+++ desktop/src/com/code2play/quickout/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.code2play.quickout.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.code2play.quickout.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
